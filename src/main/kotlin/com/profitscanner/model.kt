42639ec8958b002797416d397bfac002e70faaa7
root <localducks@gmail.com>
2019-09-22 18:51:55 +0300

    percent property

--- src/main/kotlin/com/profitscanner/model.kt
+++ src/main/kotlin/com/profitscanner/model.kt
@@ -6,5 +6,13 @@ import java.util.*
 data class DivEvent(
         val ticker: String,
         val date: Date,
+        val stockPrice: BigDecimal,
         val divAmount: BigDecimal
-)
+) {
+
+  val percent: BigDecimal
+    get() {
+        return BigDecimal.ZERO
+    }
+
+}
